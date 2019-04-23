77fcd483980a4461a801aa9757316e92302a312b
mudin <muhridin@next-aeon.com>
2019-04-23 09:45:07 +0900

    version 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import Vuex from 'vuex';
+import Vuex from 'vuex'
 
-const VueGlobalVariable = { 
-  install(Vue:any, options:any) {
+const VueGlobalVariable = {
+  install(Vue: any, options: any) {
 
-    options = options || {};
+    options = options || {}
     // const store = options.store || {state:{}}
-    Vue.use(Vuex);
-    const store = new Vuex.Store({state:{}});
+    Vue.use(Vuex)
+    const store = new Vuex.Store({state: {}})
 
-    const globals = options.globals || {};
+    const globals = options.globals || {}
 
-    Object.keys(globals).forEach((key:string) => {
-      (<any>window)[`$${key}`] = globals[key];
-      Vue.set(store.state, key, globals[key]);
-    });
+    Object.keys(globals).forEach((key: string) => {
+      (window as any)[`${key}`] = globals[key]
+      Vue.set(store.state, key, globals[key])
+    })
 
-    const computed = {};
+    const computed = {}
     Object.keys(globals).forEach((key) => {
-      (<any>computed)[`$${key}`] = () => globals[key];
-    });
+      (computed as any)[`${key}`] = () => globals[key]
+    })
 
     Vue.mixin({
-      computed,
-    });
-  },
-};
+      computed
+    })
+  }
+}
 
-export default VueGlobalVariable;
+export default VueGlobalVariable
